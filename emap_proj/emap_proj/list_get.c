49944a00f5130a1d4a59d9d30d50435629d82a2d
Patrick Rabier <patrick.rabier@gmail.com>
2012-11-07 11:04:51 +0800

    list search

---
+++ emap_proj/emap_proj/list_get.c
@@ -0,0 +1,52 @@
+#include "list.h"
+
+void		*List_get(List *l, int idx)
+{
+	List_Iterator *it = FIRST(l);
+
+	if (COUNT(l) < idx)
+		return NULL;
+
+	while (idx && it) {
+		--idx;
+		INC_IT(it);
+	}
+	if (it && !idx)
+		return it->data;
+	return NULL;
+}
+
+void		*List_get_first(List *l, list_predicate fct, void * param)
+{
+	List_Iterator *it = FIRST(l);
+
+	while (it)
+	{
+		if (fct(it->data, param))
+			return it->data;
+		INC_IT(it);
+	}
+	return NULL;
+}
+
+List		*List_get_all(List *l, list_predicate fct, void *param)
+{
+	List	*result_set = NEW(List);
+	List_Iterator *it = FIRST(l);
+
+	if (!result_set)
+		return NULL;
+
+	while (it)
+	{
+		if (fct(it->data, param))
+			List_append(result_set, it->data);
+		INC_IT(it);
+	}
+	if (!COUNT(result_set))
+	{
+		free(result_set);
+		return NULL;
+	}
+	return result_set;
+}
